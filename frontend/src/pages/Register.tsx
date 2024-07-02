619ec6585fa2b92e1b68d564341f124a4ce6f4e5
valentina roy <tinar478612@gmail.com>
2024-07-02 14:38:24 +0530

    fixed register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form"
 import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from '../api-client'
 import { useAppContext } from "../contexts/AppContext";
-import {  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string,
@@ -112,7 +112,10 @@ const Register =() =>{
             <span className="text-red-500">{errors.confirmPassword.message}</span>
           )}
       </label>
-      <span>
+      <span className="flex items-center justify-between ">
+        <span className="text-sm">
+          Already have an account? <Link className="underline" to="/sign-in">Sign In</Link>
+        </span>
         <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Create Account</button>
       </span>
     </form>
